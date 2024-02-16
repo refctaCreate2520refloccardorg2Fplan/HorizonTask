8f82c8e4d6669d36ba1b949d494dafd856577308
Marko Jantula <jantulamarko@outlook.com>
2024-02-16 10:05:34 +0100

    [+] task controller, DTO

---
+++ HorizonTask/Controllers/TaskController.cs
@@ -0,0 +1,35 @@
+﻿using HorizonTask.Data;
+using HorizonTask.DTO;
+using HorizonTask.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HorizonTask.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TasksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<TasksDTO> GetTasksList()
+        {
+            IEnumerable<ApplicationTask> Tasks = _context.Tasks;
+            return Tasks.Select(Tasks => new TasksDTO
+            {
+                Id = Tasks.Id,
+                Name = Tasks.Name,
+                Description = Tasks.Description,
+                Priority = Tasks.Priority,
+                IsDone = Tasks.IsDone,
+            }
+            );
+        }
+
+    }
+}
